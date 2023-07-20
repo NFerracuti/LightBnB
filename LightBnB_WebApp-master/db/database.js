26708d9951c4b341fe6032d7cf2f33a3baaf9029
NFerracuti <npferracuti@gmail.com>
2023-07-20 01:11:32 -0400

    add the add property function to database.js

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -23,7 +23,6 @@ const getUserWithEmail = function (email) {
     .query(`
     SELECT * FROM users WHERE email = '${email}';`)
     .then((result) => {
-      console.log(result.rows);
       return result.rows[0];
     })
     .catch((err) => {
@@ -41,7 +40,6 @@ const getUserWithId = function (id) {
     .query(`
     SELECT * FROM users WHERE id = '${id}';`)
     .then((result) => {
-      console.log(result.rows);
       return result.rows[0];
     })
     .catch((err) => {
@@ -64,9 +62,12 @@ const addUser = function (user) {
       '${user.password}')
       RETURNING *;`
       )
-    // .then((result) => {
-    //   return result.rows[0];
-    // })
+    .then((res) => {
+      if(!res.rows.length){
+        return(null)
+      }
+      return res.rows[0];
+      })
     .catch((err) => {
       console.log(err.message);
     });
@@ -94,7 +95,6 @@ const getAllReservations = function (guest_id, limit = 10) {
     ORDER BY start_date;
     `)
     .then((result) => {
-      console.log(result.rows);
       return result.rows[0];
     })
     .catch((err) => {
@@ -172,9 +172,6 @@ const getAllProperties = function (options, limit = 10) {
   LIMIT $${queryParams.length};
   `;
 
-  // 5
-  console.log(queryString, queryParams);
-
   // 6
   return pool.query(queryString, queryParams).then((res) => res.rows);
 };
@@ -185,10 +182,37 @@ const getAllProperties = function (options, limit = 10) {
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const queryParams = [
+    property.owner_id, 
+    property.title,
+    property.description,
+    property.thumbnail_photo_url,
+    property.cover_photo_url,
+    property.cost_per_night,
+    property.street,
+    property.city,
+    property.province,
+    property.post_code,
+    property.country,
+    property.parking_spaces,
+    property.number_of_bathrooms,
+    property.number_of_bedrooms
+  ];
+  const queryString = `
+  INSERT INTO  properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+  RETURNING *;`
+
+  return pool
+  .query(
+    queryString,
+    queryParams)
+  .then((result) => {
+    return result.rows;
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 };
 
 module.exports = {
